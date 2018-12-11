03ee3b79a429836d554381d14c096033c42f0c10
Xealia <lillylar93@outlook.com>
2018-12-11 21:19:49 +0100

    Added geolocation

--- GeoLocationAppNew/GeoLocationAppNew/MainPage.xaml.cs
+++ GeoLocationAppNew/GeoLocationAppNew/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace GeoLocationAppNew
 {
@@ -12,6 +13,32 @@ namespace GeoLocationAppNew
         public MainPage()
         {
             InitializeComponent();
+        }
+        public async void GetCurrantLocation(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    LatitudeLabel.Text = location.Latitude.ToString();
+                    LongitudeLabel.Text = location.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
         }
     }
 }
